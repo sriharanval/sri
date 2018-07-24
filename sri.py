704ea90faf95dc61ba5e0bc98d1a6a879576300b
sriharanval <36755406+sriharanval@users.noreply.github.com>
2018-07-24 10:45:05 +0530

    Update sri.py

--- sri.py
+++ sri.py
@@ -1,1 +1,13 @@
-import java.io.; import java.util.; class vowels { publi static void main(String args[]) { Scanner s=new Scanner(System.in); char[] arr=new int{'a','e','i','o','u'}; char c=s.next(); if(c==arr) { System.out.println("vowels"); } else { System.out.println("consonants") } } }
+import java.io.; 
+import java.util.; 
+class vowels { publi static void main(String args[]) 
+{
+ Scanner s=new Scanner(System.in); 
+ char[] arr=new int{'a','e','i','o','u'};
+ char c=s.next();
+ if(c==arr) { System.out.println("vowels"); 
+ } 
+ else { System.out.println("consonants")
+ }
+ }
+ }
