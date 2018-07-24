526b43d52b8e1732f0b0967a4a7e6fcd2e324989
sriharanval <36755406+sriharanval@users.noreply.github.com>
2018-07-24 09:43:14 +0530

    sri.py

---
+++ sri.py
@@ -0,0 +1,6 @@
+testing_variable = input ("enter a number:")
+# if a string is entered print invalid
+if testing_variable == "":
+# the "" is what im using if a string is entered and im having a hard time solving this 
+#Tips or advice of any coversion of the input would be helpful 
+    print "invalid"
