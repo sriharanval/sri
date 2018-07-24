1484a2d4b088b22f84bebd1fcc50504ed1e8d107
sriharanval <36755406+sriharanval@users.noreply.github.com>
2018-07-24 10:42:40 +0530

    Update sri.py

--- sri.py
+++ sri.py
@@ -1,6 +1,1 @@
-testing_variable = input ("enter a number:")
-# if a string is entered print invalid
-if testing_variable == "":
-# the "" is what im using if a string is entered and im having a hard time solving this 
-#Tips or advice of any coversion of the input would be helpful 
-    print "invalid"
+import java.io.; import java.util.; class vowels { publi static void main(String args[]) { Scanner s=new Scanner(System.in); char[] arr=new int{'a','e','i','o','u'}; char c=s.next(); if(c==arr) { System.out.println("vowels"); } else { System.out.println("consonants") } } }
